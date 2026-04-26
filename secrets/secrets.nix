f7f8f0434b39449cde0d8aa54e04e810522379e7
aquanjsw <zhdlcc@gmail.com>
2026-04-26 08:51:20 +0800

    chore: clean secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,18 +1,19 @@
 let
   minimal-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJcbO0qdMXLqVo1um8dsJ5AsNop6f82DuHVgHfhmpV1";
-  bwh-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENvcZL6L6QpDotsU6xgClQ4f16NhUOoCIFr7lOXOLVk";
-
   minimal-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIW9TVyWzguSLNoL/PRKtYlCY0Weh1s5NZLPmSLikVHb";
-  vultr-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+XW58VGYhubgLiluoQIvN7gJoLlOxCMQmq6ff6Gk+U";
+
+  bwh-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENvcZL6L6QpDotsU6xgClQ4f16NhUOoCIFr7lOXOLVk";
   bwh-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEpzIcG2uFa8DIdHFgp9bHp9msFExzUYsilAmUnBTQuO";
+
+  users = [ minimal-user bwh-user ];
 in {
-  "caddy-env.age".publicKeys = [ minimal-user vultr-host bwh-host bwh-user ];
-  "vless-uuid.age".publicKeys = [ minimal-user minimal-host vultr-host bwh-host bwh-user ];
-  "reality-public-key.age".publicKeys = [ minimal-user minimal-host vultr-host bwh-host bwh-user ];
-  "reality-private-key.age".publicKeys = [ minimal-user minimal-host vultr-host bwh-host bwh-user ];
-  "clash-api-secret.age".publicKeys = [ minimal-user minimal-host vultr-host bwh-host bwh-user ];
-  "domain.age".publicKeys = [ minimal-user minimal-host bwh-user bwh-host ];
-  "inbound-password.age".publicKeys = [ minimal-user minimal-host vultr-host ];
+  "caddy-env.age".publicKeys = users ++ [ bwh-host ];
+  "vless-uuid.age".publicKeys = users ++ [ minimal-host bwh-host ];
+  "reality-public-key.age".publicKeys = users ++ [ minimal-host bwh-host ];
+  "reality-private-key.age".publicKeys = users ++ [ minimal-host bwh-host ];
+  "clash-api-secret.age".publicKeys = users ++ [ minimal-host bwh-host ];
+  "domain.age".publicKeys = users ++ [ minimal-host bwh-host ];
+  "inbound-password.age".publicKeys = users ++ [ minimal-host bwh-host ];
 }
 
 # vim: sts=2 sw=2 et ai
